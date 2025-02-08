237f052413b1c39ff38c3c34ec0e1f42128f05f7
Nathan Hunt <nathan.taylor.hunt@gmail.com>
2025-02-07 19:04:31 -0500

    add repro steps

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,27 @@ use bevy::prelude::*;
 use bevy_steamworks::*;
 
 fn main() {
-    App::new()
-      // it is important to add the plugin before `RenderPlugin` that comes with `DefaultPlugins`
-      .add_plugins(SteamworksPlugin::init_app(480).unwrap())
-      .add_plugins(DefaultPlugins)
-      .run();
+  App::new()
+    // it is important to add the plugin before `RenderPlugin` that comes with `DefaultPlugins`
+    .add_plugins(SteamworksPlugin::init_app(480).unwrap())
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, spawn_text)
+    .run();
+}
+
+fn spawn_text(
+  mut commands: Commands,
+) {
+  commands.spawn(Camera2dBundle::default());
+
+  commands.spawn(
+    TextBundle::from_section(
+      "Repro steps:\n\n1. Plug in controller.\n2. Open Steam overlay and enable Steam Input\n3. Go to templates and select generic game pad.\n4. Close steam overlay controller.\n5. Unplug controller.", TextStyle::default()
+    ).with_style(Style {
+      position_type: PositionType::Absolute,
+      justify_self: JustifySelf::Center,
+      align_self: AlignSelf::Center,
+      ..default()
+    })
+  );
 }
